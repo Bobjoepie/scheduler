176f069887ebedcc185f853e5b639566415365d6
Bobjoepie <16999496+Bobjoepie@users.noreply.github.com>
2021-05-25 12:58:26 -0400

    Add project files.

---
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class SqlConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - Create the SQL Connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+
+            if (textFiles)
+            {
+                // TODO - Create the Text Connection
+                TextConnector text = new TextConnector();
+                Connections.Add(text);
+            }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
